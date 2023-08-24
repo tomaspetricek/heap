aab9c754f7e14edd3d8b13be769314b1bc472391
TomasPetricek <45512445+tomaspetricek@users.noreply.github.com>
2023-08-24 09:06:39 +0200

    Implement using standard methods

--- main.cpp
+++ main.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
+#include "priority_queue.hpp"
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    top::priority_queue<int> queue;
+    queue.push(1);
+    queue.push(2);
+    queue.push(3);
+    std::cout << queue.top() << std::endl;
+    queue.pop();
+    std::cout << queue.top() << std::endl;
+    queue.pop();
+    std::cout << queue.top() << std::endl;
     return 0;
 }
--- priority_queue.hpp
+++ priority_queue.hpp
@@ -6,57 +6,85 @@
 #define HEAP_PRIORITY_QUEUE_HPP
 
 #include <vector>
+#include <algorithm>
 
-template<class T, class Container = std::vector<T>, class Compare = std::less<class Container::value_type>>
-class priority_queue {
-    Container data_;
-
-public:
-    using value_type = T;
-    using container_type = Container;
-    using size_type = std::size_t;
-
-    explicit priority_queue(const Compare& comp = Compare{}, const Container& container = Container{})
+namespace top {
+    template<class RandomIterator, class Compare>
+    void make_heap(RandomIterator first, RandomIterator last, Compare comp)
     {
 
     }
 
-    template<class InputIterator>
-    priority_queue(InputIterator first, InputIterator last, const Compare& comp)
+    template<class RandomIterator, class Compare>
+    void push_heap(RandomIterator first, RandomIterator last, Compare comp)
     {
-
+        // move upwards
     }
 
-    const T& top() const
+    template<class RandomIterator, class Compare>
+    void pop_heap(RandomIterator first, RandomIterator last, Compare comp)
     {
-
+//        data_.front() = data_.back();
+//        data_.pop_back();
+        // move downwards
     }
 
-    bool empty() const
-    {
-        return data_.empty();
-    }
+    template<class T, class Container = std::vector<T>, class Compare = std::less<typename Container::value_type>>
+    class priority_queue {
+        Compare comp_;
+        Container data_;
 
-    void push(const T& val)
-    {
+    public:
+        using value_type = T;
+        using container_type = Container;
+        using size_type = std::size_t;
 
-    }
+        explicit priority_queue(const Compare& comp = Compare{}, const Container& data = Container{})
+                :comp_{comp}, data_{data}
+        {
+            // heapify
+            std::make_heap(data_.begin(), data_.end(), comp_);
+        }
 
-    template<class... Args>
-    void emplace(Args&& ... args)
-    {
+        const T& top() const
+        {
+            // undefined behaviour if it's empty
+            return data_.front();
+        }
 
-    }
+        bool empty() const
+        {
+            return data_.empty();
+        }
 
-    void pop()
-    {
+        void push(const T& val)
+        {
+            emplace(val);
+        }
 
-    }
+        template<class... Args>
+        void emplace(Args&& ... args)
+        {
+            data_.emplace_back(std::forward<Args>(args)...);
+            std::push_heap(data_.begin(), data_.end(), comp_);
+        }
 
-    void swap(priority_queue& other) noexcept
-    {
+        void pop()
+        {
+            std::pop_heap(data_.begin(), data_.end(), comp_);
+            data_.pop_back();
+        }
 
-    }
-};
+        void swap(priority_queue& other) noexcept
+        {
+            std::swap(other.data_, data_);
+        }
+
+        size_type size() const
+        {
+            return data_.size();
+        }
+    };
+}
 
 #endif //HEAP_PRIORITY_QUEUE_HPP
